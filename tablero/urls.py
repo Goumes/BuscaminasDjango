f3c923031e93975ea9f2101bca3c6453963190d4
Goumes <alejandro_golivera@outlook.com>
2023-10-24 14:09:37 +0200

    Casi arreglado

--- tablero/urls.py
+++ tablero/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('tablero_create', views.tablero_create, name='tablero_create'),
-    path('tablero_show?columnas=<int:columnas>&filas=<int:filas>/', views.tablero_show, name='tablero_show'),
+    path('tablero_show', views.tablero_show, name='tablero_show'),
 ]
--- tablero/views.py
+++ tablero/views.py
@@ -11,9 +11,12 @@ def tablero_create(request):
             cd = form.cleaned_data
             columnas = cd.get('columnas')
             filas = cd.get('filas')
-            return redirect('tablero_show', columnas=columnas, filas=filas)
+            return redirect('tablero_show'+'?columnas='+str(columnas)+'&filas='+str(filas))
     else:
-        form = CrearTablero()
+        if request.method == 'GET' and request.GET.get('columnas', '') and request.GET.get('filas', ''):
+            return redirect('tablero_show'+'?columnas='+request.GET.get('columnas', '')+'&filas='+request.GET.get('filas', ''))
+        else:
+            form = CrearTablero()
 
     return render(request, 'tablero/tablero_create.html', {'form': form})
 
@@ -21,5 +24,8 @@ def tablero_create(request):
 def index(request):
     return render(request, 'tablero/index.html', {})
 
-def tablero_show(request, columnas, filas):
+def tablero_show(request):
+    columnas = request.GET.get('columnas', '')
+    filas = request.GET.get('filas', '')
+
     return render(request, 'tablero/tablero_show.html', {'columnas': columnas, 'filas': filas})
