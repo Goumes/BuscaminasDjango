d5345fb59fb36b43a7c7a86a653b2d0aaf9f192e
Goumes <alejandro_golivera@outlook.com>
2023-10-25 11:12:19 +0200

    arreglado

--- tablero/forms.py
+++ tablero/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class CrearTablero(forms.Form):
-    columnas = forms.IntegerField()
-    filas = forms.IntegerField()
+    columnas = forms.IntegerField(label='columnas', min_value=1, max_value=15)
+    filas = forms.IntegerField(label='filas', min_value=1, max_value=20)
--- tablero/views.py
+++ tablero/views.py
@@ -5,18 +5,14 @@ from .forms import CrearTablero
 # Create your views here.
 
 def tablero_create(request):
-    if request.method == 'POST':
-        form = CrearTablero(request.POST)
+    if request.method == 'GET' and request.GET.get('columnas', ''):
+        form = CrearTablero(request.GET)
         if form.is_valid():
-            cd = form.cleaned_data
-            columnas = cd.get('columnas')
-            filas = cd.get('filas')
-            return redirect('tablero_create'+'?columnas='+str(columnas)+'&filas='+str(filas))
-    else:
-        if request.method == 'GET' and request.GET.get('columnas', '') and request.GET.get('filas', ''):
-            return redirect('tablero_show'+'?columnas='+request.GET.get('columnas', '')+'&filas='+request.GET.get('filas', ''))
-        else:
-            form = CrearTablero()
+            columnas = request.GET.get('columnas', '')
+            filas = request.GET.get('filas', '')
+            return render(request, 'tablero/tablero_show.html', {'columnas': columnas, 'filas': filas})
+    elif request.method == 'GET':
+        form = CrearTablero()
 
     return render(request, 'tablero/tablero_create.html', {'form': form})
 
